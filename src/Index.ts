83cb4e02c5ca5abf42c2085030607ce53a9fb111
Tolfx <matteus12122@gmail.com>
2021-04-14 10:10:30 +0200

    Modified index.ts

--- src/Index.ts
+++ src/Index.ts
@@ -7,7 +7,13 @@ import flash from "connect-flash";
 import method_override from "method-override";
 import express_session from "express-session";
 
+/**
+ * Import routes here
+ */
+import MainRoute from "./Routes/Main";
+
 const app = express();
+const PORT = process.env.PORT ?? 3000;
 
 app.use(expressLayout);
 app.set('view engine', 'ejs');
@@ -39,10 +45,6 @@ app.use(passport.session());
 
 app.use(flash());
 
-app.get("/", (req, res) => {
-    res.status(200).json({
-        msg: "Hello world"
-    });
-});
+app.use("/", MainRoute);
 
-app.listen(3000, () => console.log("Open and clear"));
+app.listen(PORT, () => console.log(`Opened on port: ${PORT}`));
