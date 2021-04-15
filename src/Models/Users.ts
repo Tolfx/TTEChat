25e0210ccd4624e649a1ccb6a248b848cbea4fdf
Tolfx <matteus12122@gmail.com>
2021-04-15 08:41:07 +0200

    Modified User Schema

--- src/Models/Users.ts
+++ src/Models/Users.ts
@@ -2,6 +2,31 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
 
+    username: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    profile_picture: {
+        type: String,
+        required: true,
+    },
+
+    phone: {
+        type: String,
+        required: false,
+    },
+    
 });
 
 const User = mongoose.model("users", UserSchema);
