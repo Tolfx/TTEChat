6712868113188a1e1575bd1aa1ec75f7f6b775bf
Tolfx <matteus12122@gmail.com>
2021-05-06 08:53:40 +0200

    Added IO

--- src/Routes/Chat.ts
+++ src/Routes/Chat.ts
@@ -1,18 +1,22 @@
 import { Application, Router } from "express";
 import EnsureAuth from "../Middlewares/EnsureAuth";
+import { Server } from "socket.io";
 
 // @Tolfx
 export default class ChatRouter
 {
     protected router: Router;
     protected app: Application;
+    protected io: Server;
 
     public constructor(
         app: Application,
+        io: Server
     )
     {
         this.app = app;
         this.router = Router();
+        this.io = io;
 
         this.app.use("/chat", EnsureAuth, this.router);
 
